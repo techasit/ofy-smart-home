69b4fa3c247a67dc815d29ed1e8435695610d274
Jiraphat Siriwasinchai <jiraphat.s@om4you.com>
2018-06-12 03:35:25 +0700

    - Create new projects

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TouchMyHee
+ * Date: 6/12/2018
+ * Time: 3:31 AM
+ */
+
+echo "OFY Smart Home";
+
+?>
---
+++ javab_bot.php
@@ -0,0 +1,77 @@
+<?php
+
+$API_URL = 'https://api.line.me/v2/bot/message/reply';
+$ACCESS_TOKEN = 'jqcef/Cwv9mlugBqub57sx4Pa+LcB4wGBtf9f92NLuFqEN92x/g4M9Mj1j+DILZWgzCdj11gRgSVbTt3cTjOf+xNKgXrBmDylAor1n7fOb64X90b+VSD+c/K3ylq3UidMhttW8aJjZBvgk9hEhdNlgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+if ( sizeof($request_array['events']) > 0 )
+{
+
+    foreach ($request_array['events'] as $event)
+    {
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
+
+        if ( $event['type'] == 'message' )
+        {
+            if( $event['message']['type'] == 'text' )
+            {
+                $text = $event['message']['text'];
+                $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
+
+//                if($text == 'รถมีทั้งหมดกี่รุ่น')
+//                {
+//                    $reply_message = 'คุณต้องการถามถึงรถรุ่น Yaris หรือ Yaris ATIV?';
+//
+//                }else if($text == 'Yaris')
+//                {
+//                    $reply_message = 'มีทั้งหมด 4 รุ่น ดังนี้';
+//
+//                }else if($text == 'Yaris ATIV')
+//                {
+//                    $reply_message = 'มีทั้งหมด 5 รุ่น ดังนี้ xxxxxx';
+//                }
+
+            }
+            else
+                $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+
+        }
+        else
+            $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+
+        if( strlen($reply_message) > 0 )
+        {
+            //$reply_message = iconv("tis-620","utf-8",$reply_message);
+            $data = [
+                'replyToken' => $reply_token,
+                'messages' => [['type' => 'text', 'text' => $reply_message]]
+            ];
+            $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+            $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+            echo "Result: ".$send_result."\r\n";
+        }
+    }
+}
+
+echo "OK";
+
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+?>
